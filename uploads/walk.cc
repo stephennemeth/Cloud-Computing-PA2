a321c56f69ebed93cb2d2f5219ccb7cffe18fe1f
Ubuntu <ubuntu@ip-172-31-28-112.ec2.internal>
2023-02-26 01:11:29 +0000

    Final changes to the code

--- uploads/walk.cc
+++ uploads/walk.cc
@@ -5,7 +5,7 @@ using std::cout;
 using std::string;
 
 int main() {
-    string name1,name2
+    string name1,name2;
     std::cout << "Enter a name:";
     getline(std::cin,name1);
     std::cout << "Enter a name:";
