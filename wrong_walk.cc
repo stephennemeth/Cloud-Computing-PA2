580c01267d9f8c4c8cb4ff1d4596960c16892e3c
stephennemeth <stephennemeth4@gmail.com>
2023-02-25 20:01:57 -0500

    Added some stuff

--- wrong_walk.cc
+++ wrong_walk.cc
@@ -10,5 +10,6 @@ int main() {
     getline(std::cin,name1);
     std::cout << "Enter a name:";
     getline(std::cin,name2);
-    std::cout << name1 + " and " + name2 + " went for a walk.\n" ;
+    std::cout << name2 + " and " + name2 + " went for a walk.\n" ;
+    std::cout << "This is the wrong submission file, but has the same name walk.cc, and outputs name2 twice so score should be 1 out of 2" << std::endl;
 }
